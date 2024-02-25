c470b33315baf4fae896825b8020615dafd9fe85
Christopher Strack <christopher.strack@gmail.com>
2024-02-25 19:13:14 +0100

    Initial commit

---
+++ src/parsing.cpp
@@ -0,0 +1,93 @@
+#include "spymarine/parsing.hpp"
+
+namespace spymarine {
+
+bool operator==(const Header &lhs, const Header &rhs) {
+  return lhs.type == rhs.type && lhs.length == rhs.length;
+}
+
+bool operator!=(const Header &lhs, const Header &rhs) { return !(lhs == rhs); }
+
+bool operator==(const Message &lhs, const Message &rhs) {
+  return lhs.type == rhs.type &&
+         std::memcmp(lhs.data.data(), rhs.data.data(),
+                     std::min(lhs.data.size(), rhs.data.size())) == 0;
+}
+
+bool operator!=(const Message &lhs, const Message &rhs) {
+  return !(lhs == rhs);
+}
+
+namespace {
+
+int16_t toInt16(const std::span<const uint8_t, 2> data) {
+  return int16_t((data[0] << 8) | data[1]);
+}
+
+} // namespace
+
+std::optional<Header> parseHeader(const std::span<const uint8_t> data) {
+  if (data.size() < headerLength) {
+    return std::nullopt;
+  }
+
+  if (std::memcmp(data.data(), "\x00\x00\x00\x00\x00\xff", 6) != 0) {
+    return std::nullopt;
+  }
+
+  if (std::memcmp(data.data() + 7, "\x85\xde\xc3\x46", 4) != 0) {
+    return std::nullopt;
+  }
+
+  if (data[13] != 0xff) {
+    return std::nullopt;
+  }
+
+  const auto type = data.data()[6];
+  const auto length = toInt16(data.subspan<11, 2>());
+
+  return Header{type, length};
+}
+
+uint16_t crc(const std::span<const uint8_t> bytes) {
+  const uint16_t poly = 0x1189;
+  uint16_t crc = 0;
+
+  for (auto byte : bytes) {
+    crc ^= byte << 8;
+    for (int i = 0; i < 8; i++) {
+      if (crc & 0x8000) {
+        crc = (crc << 1) ^ poly;
+      } else {
+        crc <<= 1;
+      }
+    }
+  }
+
+  return crc;
+}
+
+std::optional<Message>
+parseResponse(const std::span<const uint8_t> rawResponse) {
+  const auto header = parseHeader(rawResponse);
+  const auto dataLength = rawResponse.size() - headerLength + 1;
+
+  if (header->length != dataLength) {
+    return std::nullopt;
+  }
+
+  const auto calculatedCrc =
+      crc(std::span{rawResponse.begin() + 1, rawResponse.end() - 3});
+  const auto receivedCrc =
+      toInt16(std::span<const uint8_t, 2>{rawResponse.end() - 2, 2});
+
+  if (calculatedCrc != receivedCrc) {
+    return std::nullopt;
+  }
+
+  return Message{
+      static_cast<MessageType>(header->type),
+      std::span{rawResponse.begin() + headerLength, rawResponse.end() - 2}};
+}
+
+} // namespace spymarine
---
+++ test/tst_parsing.cpp
@@ -0,0 +1,80 @@
+#include "spymarine/parsing.hpp"
+
+#include <catch2/catch_all.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+namespace spymarine {
+namespace {
+
+using HeaderBytes = std::array<uint8_t, headerLength>;
+
+} // namespace
+
+TEST_CASE("parseHeader") {
+  SECTION("returns Header for valid bytes") {
+    const auto [bytes, expectedHeader] = GENERATE(
+        std::make_tuple(HeaderBytes{0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x41,
+                                    0x85, 0xde, 0xc3, 0x46, 0x01, 0x61, 0xff},
+                        Header{0x41, 353}),
+        std::make_tuple(HeaderBytes{0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xb0,
+                                    0x85, 0xde, 0xc3, 0x46, 0x01, 0x14, 0xff},
+                        Header{0xb0, 276}),
+        std::make_tuple(HeaderBytes{0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x02,
+                                    0x85, 0xde, 0xc3, 0x46, 0x00, 0x11, 0xff},
+                        Header{0x02, 17}));
+
+    CHECK(parseHeader(bytes) == expectedHeader);
+  }
+
+  SECTION("returns nullopt if not enough bytes were provided") {
+    constexpr auto validBytes =
+        HeaderBytes{0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x41,
+                    0x85, 0xde, 0xc3, 0x46, 0x01, 0x61, 0xff};
+    CHECK(parseHeader(std::span{validBytes}.subspan(0, 13)) == std::nullopt);
+  }
+
+  SECTION("returns nullopt if header doesn't match pattern") {
+    auto bytes = HeaderBytes{0x01, 0x00, 0x00, 0x00, 0x00, 0xff, 0x41,
+                             0x85, 0xde, 0xc3, 0x46, 0x01, 0x61, 0xff};
+    CHECK(parseHeader(bytes) == std::nullopt);
+  }
+}
+
+TEST_CASE("parseResponse") {
+  const auto validResponse = std::array<uint8_t, 30>{
+      0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x02, 0x85, 0xde, 0xc3,
+      0x46, 0x00, 0x11, 0xff, 0x01, 0x01, 0x00, 0x00, 0x00, 0x1f,
+      0xff, 0x02, 0x01, 0x00, 0x00, 0x00, 0x29, 0xff, 0x13, 0x14};
+
+  SECTION("returns message for valid response") {
+    const auto expectedData =
+        std::array<uint8_t, 14>{0x01, 0x01, 0x00, 0x00, 0x00, 0x1f, 0xff,
+                                0x02, 0x01, 0x00, 0x00, 0x00, 0x29, 0xff};
+    const auto expectedMessage = Message{
+        MessageType::deviceCount,
+        expectedData,
+    };
+    CHECK(parseResponse(validResponse) == expectedMessage);
+  }
+
+  SECTION("returns nullopt if length is invalid") {
+    auto invalidResponse = validResponse;
+    invalidResponse[12] += 1;
+    CHECK(parseResponse(invalidResponse) == std::nullopt);
+  }
+
+  SECTION("returns nullopt if crc is invalid") {
+    auto invalidResponse = validResponse;
+    invalidResponse[invalidResponse.size() - 1] += 1;
+    CHECK(parseResponse(invalidResponse) == std::nullopt);
+  }
+}
+
+TEST_CASE("crc") {
+  const auto message = HeaderBytes{0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x02,
+                                   0x04, 0x8C, 0x55, 0x4B, 0x00, 0x03, 0xFF};
+  const auto span = std::span{message.begin() + 1, message.size() - 2};
+  CHECK(crc(span) == 43200);
+}
+
+} // namespace spymarine
