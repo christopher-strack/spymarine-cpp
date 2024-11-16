e12ef0f50b9eb51bac5d6ea093f09001c5532e15
Christopher Strack <christopher.strack@gmail.com>
2024-11-16 16:27:37 +0100

    Add benchmark for read_devices

--- test/tst_client.cpp
+++ test/tst_client.cpp
@@ -53,36 +53,45 @@ private:
 } // namespace
 
 TEST_CASE("client") {
-  client<mock_tcp_socket> client{0, 0, std::chrono::seconds{0}};
-  std::vector<device> devices;
-  REQUIRE(client.read_devices(devices));
+  SECTION("read_devices") {
+    client<mock_tcp_socket> client{0, 0, std::chrono::seconds{0}};
+    std::vector<device> devices;
+    REQUIRE(client.read_devices(devices));
 
-  const auto expected_devices = std::vector<device>{
-      barometer_device{"Barometer", 3},
-      pico_internal_device{5},
-      voltage_device{"ST107 [5596] 1", 11},
-      voltage_device{"ST107 [5596] 2", 12},
-      voltage_device{"ST107 [5596] 3", 13},
-      resistive_device{"ST107 [5596] 1", 14},
-      resistive_device{"ST107 [5596] 2", 15},
-      resistive_device{"ST107 [5596] 3", 16},
-      resistive_device{"ST107 [5596] 4", 17},
-      current_device{"SC303 [5499]", 19},
-      voltage_device{"SC303 [5499] 1", 21},
-      voltage_device{"SC303 [5499] 2", 22},
-      resistive_device{"SC303 [5499] 1", 23},
-      resistive_device{"SC303 [5499] 2", 24},
-      resistive_device{"SC303 [5499] 3", 25},
-      battery_device{"Bulltron", battery_type::lifepo4, 300.0f, 26},
-      temperature_device{"Batterie", 31},
-      tank_device{"Frischwasser", fluid_type::fresh_water, 100.0f, 32},
-      battery_device{"Starterbatterie", battery_type::agm, 100.0f, 33},
-      tank_device{"Abwasser", fluid_type::waste_water, 70.0f, 38},
-      temperature_device{"Innen", 39},
-      temperature_device{"Au\xa3\x65n ", 40},
-      temperature_device{"Boiler", 41},
+    const auto expected_devices = std::vector<device>{
+        barometer_device{"Barometer", 3},
+        pico_internal_device{5},
+        voltage_device{"ST107 [5596] 1", 11},
+        voltage_device{"ST107 [5596] 2", 12},
+        voltage_device{"ST107 [5596] 3", 13},
+        resistive_device{"ST107 [5596] 1", 14},
+        resistive_device{"ST107 [5596] 2", 15},
+        resistive_device{"ST107 [5596] 3", 16},
+        resistive_device{"ST107 [5596] 4", 17},
+        current_device{"SC303 [5499]", 19},
+        voltage_device{"SC303 [5499] 1", 21},
+        voltage_device{"SC303 [5499] 2", 22},
+        resistive_device{"SC303 [5499] 1", 23},
+        resistive_device{"SC303 [5499] 2", 24},
+        resistive_device{"SC303 [5499] 3", 25},
+        battery_device{"Bulltron", battery_type::lifepo4, 300.0f, 26},
+        temperature_device{"Batterie", 31},
+        tank_device{"Frischwasser", fluid_type::fresh_water, 100.0f, 32},
+        battery_device{"Starterbatterie", battery_type::agm, 100.0f, 33},
+        tank_device{"Abwasser", fluid_type::waste_water, 70.0f, 38},
+        temperature_device{"Innen", 39},
+        temperature_device{"Au\xa3\x65n ", 40},
+        temperature_device{"Boiler", 41},
+    };
+    CHECK(devices == expected_devices);
+  }
+
+  BENCHMARK("read_devices") {
+    client<mock_tcp_socket> client{0, 0, std::chrono::seconds{0}};
+    std::vector<device> devices;
+    [[maybe_unused]] const auto result = client.read_devices(devices);
+    return devices;
   };
-  CHECK(devices == expected_devices);
 }
 
 } // namespace spymarine
