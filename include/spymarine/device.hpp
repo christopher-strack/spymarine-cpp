ccc485a8cab9d43bac8b4337b6532ded52b04a28
Christopher Strack <christopher.strack@gmail.com>
2024-11-17 18:11:03 +0100

    Move implementation to cpp

--- include/spymarine/device.hpp
+++ include/spymarine/device.hpp
@@ -178,47 +178,8 @@ template <typename T> sensor_map build_sensor_map(T& devices_range) {
   return result;
 }
 
-inline float sensor_value(numeric_value value, sensor_type type) {
-  switch (type) {
-  case sensor_type::volume:
-    return value.second() / 10.0;
-  case sensor_type::level:
-    return value.first() / 10.0;
-  case sensor_type::voltage:
-    return value.second() / 1000.0f;
-  case sensor_type::current:
-    return value.second() / 100.0f;
-  case sensor_type::temperature:
-    return value.second() / 10.0f;
-  case sensor_type::pressure:
-    return value.number();
-  case sensor_type::resistive:
-    return value.second();
-  case sensor_type::charge:
-    return value.first() / 160.0;
-  case sensor_type::capacity:
-    return value.second() / 100.0;
-  }
+float sensor_value(numeric_value value, sensor_type type);
 
-  return 0.0f;
-}
-
-inline void update_sensor_states(message state_message, sensor_map& map) {
-  if (state_message.type != message_type::sensor_state) {
-    return;
-  }
-
-  message_values_view state_values{state_message.data};
-
-  for (const auto& entry : state_values) {
-    if (const auto value = std::get_if<numeric_value>(&entry.value)) {
-      if (const auto it = map.find(entry.id); it != map.end()) {
-        for (sensor_info* sensor : it->second) {
-          sensor->value = sensor_value(*value, sensor->type);
-        }
-      }
-    }
-  }
-}
+void update_sensor_states(message state_message, sensor_map& map);
 
 } // namespace spymarine
--- src/device.cpp
+++ src/device.cpp
@@ -41,4 +41,47 @@ battery_device::battery_device(std::string name, battery_type type,
       current_sensor{sensor_type::current, uint8_t(state_start_index + 1)},
       voltage_sensor{sensor_type::voltage, uint8_t(state_start_index + 2)} {}
 
+float sensor_value(numeric_value value, sensor_type type) {
+  switch (type) {
+  case sensor_type::volume:
+    return value.second() / 10.0;
+  case sensor_type::level:
+    return value.first() / 10.0;
+  case sensor_type::voltage:
+    return value.second() / 1000.0f;
+  case sensor_type::current:
+    return value.second() / 100.0f;
+  case sensor_type::temperature:
+    return value.second() / 10.0f;
+  case sensor_type::pressure:
+    return value.number();
+  case sensor_type::resistive:
+    return value.second();
+  case sensor_type::charge:
+    return value.first() / 160.0;
+  case sensor_type::capacity:
+    return value.second() / 100.0;
+  }
+
+  return 0.0f;
+}
+
+void update_sensor_states(message state_message, sensor_map& map) {
+  if (state_message.type != message_type::sensor_state) {
+    return;
+  }
+
+  message_values_view state_values{state_message.data};
+
+  for (const auto& entry : state_values) {
+    if (const auto value = std::get_if<numeric_value>(&entry.value)) {
+      if (const auto it = map.find(entry.id); it != map.end()) {
+        for (sensor_info* sensor : it->second) {
+          sensor->value = sensor_value(*value, sensor->type);
+        }
+      }
+    }
+  }
+}
+
 } // namespace spymarine
