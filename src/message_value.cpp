14b429935ce9c736a8283eb7f62caa7f5bd44a74
Christopher Strack <dev@christopherstrack.com>
2025-11-08 21:26:05 +0100

    Use byte_operations.hpp in message_value.cpp

--- src/message_value.cpp
+++ src/message_value.cpp
@@ -1,4 +1,5 @@
 #include "spymarine/message_value.hpp"
+#include "spymarine/byte_operations.hpp"
 
 #include <algorithm>
 
@@ -8,9 +9,13 @@ numeric_value::numeric_value(std::span<const uint8_t, 4> bytes) {
   std::copy(bytes.begin(), bytes.end(), _bytes.begin());
 }
 
-int16_t numeric_value::first() const { return (_bytes[0] << 8) | _bytes[1]; }
+int16_t numeric_value::first() const {
+  return to_uint16(std::span{_bytes}.subspan<0, 2>());
+}
 
-int16_t numeric_value::second() const { return (_bytes[2] << 8) | _bytes[3]; }
+int16_t numeric_value::second() const {
+  return to_uint16(std::span{_bytes}.subspan<2, 2>());
+}
 
 int32_t numeric_value::number() const {
   return (_bytes[0] << 24) | (_bytes[1] << 16) | (_bytes[2] << 8) | _bytes[3];
