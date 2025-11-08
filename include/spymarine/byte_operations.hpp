74bed242f0c8b5e2a1bc1bed58067da1a28d863f
Christopher Strack <dev@christopherstrack.com>
2025-11-08 21:27:10 +0100

    Introduce to_uint32

--- include/spymarine/byte_operations.hpp
+++ include/spymarine/byte_operations.hpp
@@ -17,4 +17,15 @@ constexpr uint16_t to_uint16(const std::span<const uint8_t, 2> data) noexcept {
   }
 }
 
+constexpr uint32_t to_uint32(const std::span<const uint8_t, 4> data) noexcept {
+  const auto native_order = std::bit_cast<uint32_t, std::array<uint8_t, 4>>(
+      {data[3], data[2], data[1], data[0]});
+
+  if constexpr (std::endian::native == std::endian::big) {
+    return std::byteswap(native_order);
+  } else {
+    return native_order;
+  }
+}
+
 } // namespace spymarine
