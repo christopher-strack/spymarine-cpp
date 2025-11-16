2d6059c2c9a71833ea80df4b9e1d929e74213a4a
Christopher Strack <dev@christopherstrack.com>
2025-11-16 18:51:42 +0100

    Enable sanitizers in debug

--- cmake/clang-toolchain.cmake
+++ cmake/clang-toolchain.cmake
@@ -3,6 +3,12 @@ include_guard(GLOBAL)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-set(CMAKE_CXX_FLAGS "-stdlib=libc++")
+if (SPYMARINE_ENABLE_SANITIZERS)
+    set(SANITIZER_FLAGS
+        "-fsanitize=address -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined"
+    )
+endif()
+
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ ${SANITIZER_FLAGS}")
 
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
--- cmake/gcc-toolchain.cmake
+++ cmake/gcc-toolchain.cmake
@@ -3,4 +3,19 @@ include_guard(GLOBAL)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
 
+if (SPYMARINE_ENABLE_SANITIZERS)
+    if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "arm64|aarch64")
+        # Apple Silicon doesn't support all sanitizers with GCC
+        set(SANITIZER_FLAGS
+            "-fsanitize=address -fsanitize=undefined -fsanitize-undefined-trap-on-error"
+        )
+    else()
+        set(SANITIZER_FLAGS
+            "-fsanitize=address -fsanitize=leak -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined -fsanitize-undefined-trap-on-error"
+        )
+    endif()
+endif()
+
+set(CMAKE_CXX_FLAGS "${SANITIZER_FLAGS}")
+
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
