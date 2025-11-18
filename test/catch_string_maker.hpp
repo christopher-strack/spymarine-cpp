48ce7ecbaede52dd051469b76225e749b4e0bffb
Christopher Strack <dev@christopherstrack.com>
2025-11-18 22:32:08 +0100

    Improve StringMaker for message_value

--- test/catch_string_maker.hpp
+++ test/catch_string_maker.hpp
@@ -2,10 +2,12 @@
 
 #include "spymarine/error.hpp"
 #include "spymarine/message_value.hpp"
+#include "spymarine/overloaded.hpp"
 
 #include <catch2/catch_all.hpp>
 
 #include <expected>
+#include <format>
 
 namespace Catch {
 
@@ -22,9 +24,26 @@ template <typename T> struct StringMaker<std::expected<T, spymarine::error>> {
 template <> struct StringMaker<spymarine::message_value> {
   static std::string convert(const spymarine::message_value& value) {
     return std::visit(
-        [](const auto& v) {
-          const auto bytes = v.raw_bytes();
-          return StringMaker<decltype(bytes)>::convert(bytes);
+        spymarine::overloaded{
+            [](const spymarine::invalid_value& invalid) {
+              return std::format("invalid_value {{ id = {} }}", invalid.id());
+            },
+            [](const spymarine::numeric_value1& numeric) {
+              const auto& bytes = numeric.raw_bytes();
+              return std::format("numeric_value1 {{ id = {}, bytes = {} }}",
+                                 numeric.id(),
+                                 StringMaker<decltype(bytes)>::convert(bytes));
+            },
+            [](const spymarine::numeric_value3& numeric) {
+              const auto& bytes = numeric.raw_bytes();
+              return std::format("numeric_value3 {{ id = {}, bytes = {} }}",
+                                 numeric.id(),
+                                 StringMaker<decltype(bytes)>::convert(bytes));
+            },
+            [](const spymarine::string_value& str) {
+              return std::format(R"(string_value {{ id = {}, str = "{}" }})",
+                                 str.id(), str.str());
+            },
         },
         value);
   }
