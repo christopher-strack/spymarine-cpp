64071b8f62f8b119564f04f73cf36764dd766184
Christopher Strack <dev@christopherstrack.com>
2025-12-12 09:25:23 +0100

    Introduce hub::supported_devices()

--- include/spymarine/hub.hpp
+++ include/spymarine/hub.hpp
@@ -35,6 +35,12 @@ public:
 
   const std::vector<device2>& devices() const noexcept { return _devices; }
 
+  auto supported_devices() const noexcept {
+    return _devices | std::views::filter([](const auto& device_) {
+             return !std::holds_alternative<unsupported_device2>(device_);
+           });
+  }
+
   const std::vector<sensor2>& sensors() const noexcept { return _sensors; }
 
   auto sensors(const device2& device_) const noexcept {
